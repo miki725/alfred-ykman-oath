09f0b272d060668d435d54325fb1ea506f19f7d3
Miroslav Shubernetskiy <miroslav@miki725.com>
2021-09-13 01:14:03 -0400

    bash everything

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-yubikey-oath.alfredworkflow: get_code.sh icon.png info.plist list_codes.py
-	zip $@ $^
+release:
+		zip -r ../totp.alfredworkflow *
--- get_code.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-ykman oath accounts code "$1" | awk '{print ($NF)}' | tr -d '\n'
-osascript -e 'display notification "Copied TOTP code to clipboard" with title "ykman oath"'
